a959b8f6d517ac031bb0611275691e3a22b776af
Talmo Pereira <talmo.pereira@umbc.edu>
2015-02-24 18:25:46 -0500

    More IGC pipelining - Parse sample ORFs from FASTA of gene predictions - Get promoter sequence - Fixes and validation for PSSMScorer

--- PSSMScorer.py
+++ PSSMScorer.py
@@ -7,7 +7,7 @@ Created on Wed Feb 11 11:05:08 2015
 @author: Talmo
 """
 
-from Bio import Seq, Alphabet, motifs
+from Bio import Seq, SeqRecord, Alphabet, motifs
 import numpy as np
 
 class PSSMScorer:
@@ -16,8 +16,10 @@ class PSSMScorer:
         a path to a text file containing one site per line, or a list of 
         Biopython Bio.Seq objects."""
         
+        self.alphabet = Alphabet.IUPAC.unambiguous_dna
+        
         if type(binding_sites) == str:
-            self.seqs = [Seq.Seq(site.strip(), Alphabet.IUPAC.unambiguous_dna) for site in open(binding_sites).readlines()]
+            self.seqs = [Seq.Seq(site.strip(), self.alphabet) for site in open(binding_sites).readlines()]
         elif type(binding_sites) == list:
             self.seqs = binding_sites
             
@@ -31,9 +33,15 @@ class PSSMScorer:
         
     def convert_seq(self, seq):
         """ Converts sequence strings to Biopython Seq objects with appropriate
-        alphabet (Alphabet.IUPAC.unambiguous_dna). """
+        alphabet. """
+        
         if type(seq) == str:
-            return Seq.Seq(seq.strip(), Alphabet.IUPAC.unambiguous_dna)
+            return Seq.Seq(seq.strip(), self.alphabet)
+        elif type(seq) == SeqRecord.SeqRecord:
+            return self.convert_seq(seq.seq)
+        elif type(seq) == Seq.Seq and seq.alphabet != self.alphabet:
+            seq.alphabet = self.alphabet
+            return seq
         else:
             return seq
         
@@ -45,15 +53,15 @@ class PSSMScorer:
         
     def score_all(self, seq):
         """ Scores all sites in a sequence and returns an array of scores. """
-        seq = self.convert_seq(seq)
-        n = len(seq)
-        m = self.pssm.length
-        
-        scores = np.zeros(1, n-m+1)
-        for pos in range(0, n-m+1):
-            scores[pos] = max(self.pssm.calculate(seq[pos:pos+m]), \
-                              self.pssm_r.calculate(seq[pos:pos+m]))
-        return scores
+#        seq = self.convert_seq(seq)
+#        n = len(seq)
+#        m = self.pssm.length
+#        
+#        scores = np.zeros((1, n-m+1))
+#        for pos in range(0, n-m+1):
+#            scores[pos] = max(self.pssm.calculate(seq[pos:pos+m]), \
+#                              self.pssm_r.calculate(seq[pos:pos+m]))
+        return self.search(seq, -np.inf)
         
     def search(self, seq, threshold=0.0):
         """ Search for the sites in the sequence with a score above a threshold.
--- igc_reference.py
+++ igc_reference.py
@@ -6,12 +6,15 @@ Created on Wed Feb 18 14:21:39 2015
 
 @author: Talmo
 """
-
 import pandas as pd
+import numpy as np
+from Bio import SeqIO
+from PSSMScorer import PSSMScorer
+
 
 #%% Load data
-p = "E:\\metagenomics\\Li et al. (2014)\\3.IGC.AnnotationInfo\\IGC.annotation.summary.v2.head"
-t = pd.read_table(p, names=['gene_id', 'gene_name', 'gene_length', 'completeness', 'cohort_origin', 'phylum', 'genus', 'kegg', 'eggNOG', 'sample_freq', 'individual_freq', 'eggNOG_funccat', 'kegg_funccat', 'cohort_assembled'])
+p = "E:\\metagenomics\\Li et al. (2014)\\3.IGC.AnnotationInfo\\IGC.annotation.summary.v2"
+t = pd.read_table(p, names=['gene_id', 'gene_name', 'gene_length', 'completeness', 'cohort_origin', 'phylum', 'genus', 'kegg', 'eggNOG', 'sample_freq', 'individual_freq', 'eggNOG_funccat', 'kegg_funccat', 'cohort_assembled'], index_col='gene_id')
 
 lexA = t[t.eggNOG == 'COG1974']
 recA = t[t.eggNOG == 'COG0468']
@@ -25,7 +28,7 @@ stats['n_complete'] = sum(nog['completeness'] == 'Complete')
 stats['n_has_phylum'] = sum(nog['phylum'] != 'unknown')
 print stats
 print nog.phylum.value_counts()
-lexA.to_csv('IGC.lexA.csv')
+nog.to_csv('IGC.lexA.csv')
 
 #%% Get stats for recA
 nog = recA
@@ -36,4 +39,61 @@ stats['n_complete'] = sum(nog['completeness'] == 'Complete')
 stats['n_has_phylum'] = sum(nog['phylum'] != 'unknown')
 print stats
 print nog.phylum.value_counts()
-lexA.to_csv('IGC.recA.csv')
+nog.to_csv('IGC.recA.csv')
+
+#%% Get scaftig
+gene_name = lexA.iloc[0].gene_name
+sample, gene = gene_name.split('_')
+sample_path = "E:\\metagenomics\\Li et al. (2014)\\4.IndividualAssmeblies\\V1.CD44-0.scaftig.more500.corrected.break.fa"
+orf_path = "E:\\metagenomics\\Li et al. (2014)\\5.IndividualORFs\\V1.CD44-0.scaftig.more500.corrected.break.fa.more100.fa"
+
+# Load sample scaffolds/scafftigs
+scaffolds = SeqIO.index(sample_path, 'fasta')
+
+# Load FASTA headers for ORFs
+with open(orf_path) as f:
+    lines = [line[1:-2] for line in f if line[0] == '>']
+ORF_pattern = '(?P<gene_name>[^ ]+)[ ]+\\[(?P<orf_type>[^\]]+)\\][ ]+locus=(?P<scaffold>[^:]+):(?P<start>[^:]+):(?P<end>[^:]+):(?P<strand>[^:\[]+)\\[(?P<completeness>[^\[\]]+)'
+ORFs = pd.Series(lines).str.extract(ORF_pattern).convert_objects(convert_numeric=True)
+
+#%% Operon prediction
+threshold_IGI = 50 # max intergenic interval (bp)
+
+# - group by 'scaffold' column and do groupwise operations
+for scaffold_name, scaf_ORFs in ORFs.groupby('scaffold'):
+    if scaf_ORFs.shape[0] > 10:
+        break
+    
+    # Sort by start values
+    
+    # Compute intergenic intervals
+    IGIs = scaf_ORFs.start[1:].values - scaf_ORFs.end[:-1].values
+    
+    # Find head genes (first gene is trivially a head gene)
+    head_genes = np.hstack((True, IGIs > threshold_IGI))
+    
+    # Split at head gene indices somehow?
+    
+
+
+#%% Extract sequence data for specific gene
+# Get gene information
+gene_orf = ORFs[ORFs['gene_name'] == gene_name]
+
+# Get scaffold
+scaffold_name = gene_orf.scaffold.iat[0]
+scaffold = scaffolds[scaffold_name]
+gene_start = gene_orf.start.iat[0] - 1
+gene_end = gene_orf.end.iat[0]
+gene = scaffold[gene_start:gene_end]
+
+# Get promoter
+promoter_region = (-250, +50)
+promoter_start = max(gene_start + promoter_region[0], 0)
+promoter_end = min(gene_start + promoter_region[1], gene_end)
+promoter = scaffold[promoter_start:promoter_end]
+
+#%% Score promoter region
+lexA_sites = "E:\\metagenomics\\binding_sites\\lexA.Alphaproteobacteria.txt"
+pssm = PSSMScorer(lexA_sites)
+print pssm.search(promoter)
